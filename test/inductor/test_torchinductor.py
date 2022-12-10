adcdd91f876b0ba88e5149aefe337b7b81682173
blzheng <beilei.zheng@intel.com>
2022-12-10 02:23:00 -0800

    rename test_input_is_view to test_input_is_inplace_view

--- test/inductor/test_torchinductor.py
+++ test/inductor/test_torchinductor.py
@@ -5305,7 +5305,7 @@ if HAS_CPU:
                     kernel_profile_events.append(e.name)
             assert len(kernel_profile_events) > 0
 
-        def test_input_is_view(self):
+        def test_input_is_inplace_view(self):
             @torch._dynamo.optimize("inductor")
             def fn(a):
                 unsqueeze_ = torch.ops.aten.unsqueeze_.default(a, 0)
